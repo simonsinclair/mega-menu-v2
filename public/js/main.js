716bdee8fb2029b2b125a697b4433a2d5ae9b57f
Simon Sinclair <simon@sinclair>
2015-06-01 15:43:14 +0100

    Update

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,15 @@
 
 	var Menu = {
 
+    open: false,
+
     init: function() {
       Menu.bindEvts();
     },
 
     bindEvts: function() {
       $('a[href="#"]').on('click', false);
+      $('#js-toggle-menu').on('click', Menu.toggle);
       $('.has-children ul').hover(Menu.highlightRow, Menu.unHighlightRow);
     },
 
@@ -25,6 +28,34 @@
       $(this)
         .parent()
         .removeClass('highlight-row');
+    },
+
+    toggle: function() {
+      if( !Menu.open ) {
+
+        $('#js-toggle-menu').addClass('toggle-menu--active');
+        Menu.hidePrimaryNav();
+        $('#js-secondary-nav').addClass('secondary-nav--hidden');
+        $('#js-mega-menu').addClass('mega-menu--open');
+        Menu.open = true;
+
+      } else {
+
+        $('#js-toggle-menu').removeClass('toggle-menu--active');
+        Menu.showPrimaryNav();
+        $('#js-secondary-nav').removeClass('secondary-nav--hidden');
+        $('#js-mega-menu').removeClass('mega-menu--open');
+        Menu.open = false;
+
+      }
+    },
+
+    hidePrimaryNav: function() {
+
+    },
+
+    showPrimaryNav: function() {
+
     }
   };
 
