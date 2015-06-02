d627c7e5c31023390ca2d8ac97df217fdccdf2b3
Simon Sinclair <simon@sinclair>
2015-06-02 16:15:22 +0100

    Refer to master branch

--- js/main.js
+++ js/main.js
@@ -2,12 +2,60 @@
 //
 
 (function(w, $, undefined) {
-  "use strict";
+  'use strict';
 
 	var Menu = {
 
+    open: false,
+
     init: function() {
-      // ...
+      Menu.bindEvts();
+    },
+
+    bindEvts: function() {
+      $('a[href="#"]').on('click', false);
+      $('#js-toggle-menu').on('click', Menu.toggle);
+      $('.has-children ul').hover(Menu.highlightRow, Menu.unHighlightRow);
+    },
+
+    highlightRow: function() {
+      $(this)
+        .parent()
+        .addClass('highlight-row');
+    },
+
+    unHighlightRow: function() {
+      $(this)
+        .parent()
+        .removeClass('highlight-row');
+    },
+
+    toggle: function() {
+      if( !Menu.open ) {
+
+        $('#js-toggle-menu').addClass('toggle-menu--active');
+        Menu.hidePrimaryNav();
+        $('#js-secondary-nav').addClass('secondary-nav--hidden');
+        $('#js-mega-menu').addClass('mega-menu--open');
+        Menu.open = true;
+
+      } else {
+
+        $('#js-toggle-menu').removeClass('toggle-menu--active');
+        Menu.showPrimaryNav();
+        $('#js-secondary-nav').removeClass('secondary-nav--hidden');
+        $('#js-mega-menu').removeClass('mega-menu--open');
+        Menu.open = false;
+
+      }
+    },
+
+    hidePrimaryNav: function() {
+      $('#js-primary-nav').addClass('primary-nav--hidden');
+    },
+
+    showPrimaryNav: function() {
+      $('#js-primary-nav').removeClass('primary-nav--hidden');
     }
   };
 
